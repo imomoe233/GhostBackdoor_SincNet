21afcf021efc413a493190e9ffdd00d450d472a9
Xu Weida <imomoe233@outlook.com>
2023-07-30 19:07:06 +0800

    update

--- dnn_models.py
+++ dnn_models.py
@@ -709,6 +709,8 @@ class Backdoor_MLP(nn.Module):
                 with open('drop_neuro.txt', 'r') as file:
                     contents = file.read()
                 array = list(range(int(contents)))
+                array = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,2047,2046,2045,2044,2043,2042,2041,2040,2039,2038,2037,2036,2035,2034,2033,2032,2031,2030,2029,2028,2027,2026,2025,2024,2023]
+                
                 print(array)
                 #self.drop.append(MyDropout(inplace=True, indices=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99], p=self.fc_drop[i], attack_num=self.attack_num))
                 self.drop.append(MyDropout(inplace=True, indices=array, p=self.fc_drop[i], attack_num=self.attack_num))
